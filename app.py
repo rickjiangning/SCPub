0c207801500296f2ad35cd9af0612e96eaafd0dd
rickjiangning <60908109+rickjiangning@users.noreply.github.com>
2020-02-18 16:34:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def logout():
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     #app.run(debug=True,host='0.0.0.0', port=5000)
-    app.run('0.0.0.0') 
+    app.run(host='0.0.0.0', port=8080) 
